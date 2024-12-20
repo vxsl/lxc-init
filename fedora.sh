ed4dd97fd1f5322e7e2476d135d9252567bc895f
Kyle Grimsrud-Manz <hi@kylegrimsrudma.nz>
2024-12-20 10:34:51 -0800

    comments, misc tweaks, organization, xclip

--- fedora.sh
+++ fedora.sh
@@ -15,15 +15,29 @@ clone_if_not_exists() {
     fi
 }
 
-# timedatectl set-timezone America/Vancouver
 
+name="Kyle Grimsrud-Manz"
+email="hi@kylegrimsrudma.nz"
+timezone="America/Vancouver"
 update="sudo dnf update"
 install="sudo dnf install -yq"
+gdm_conf="/etc/gdm/custom.conf"
+desktop_file="/usr/share/xsessions/xmonad.desktop"
 SCRIPT_PATH=$(readlink -f "$0")
 DIRNAME=$(dirname "$SCRIPT_PATH")
 
-$install xorg-x11-server-Xorg
-gdm_conf="/etc/gdm/custom.conf"
+# optional init routine
+if [ "$1" = "--init" ]; then
+    if [ ! "$2" ]; then
+        echo "Please provide a timezone, ex. 'America/Vancouver'"
+        exit 1
+    fi && \
+    timedatectl set-timezone "$timezone" && \
+    $update
+fi && \
+
+# install X, disable Wayland
+$install xorg-x11-server-Xorg && \
 if [[ -f "$gdm_conf" ]]; then
     sudo sed -i '/^#*WaylandEnable/c\WaylandEnable=false' "$gdm_conf"
 else
@@ -31,10 +45,17 @@ else
     exit 1
 fi && \
 
-$update && \
-$install git tig curl zsh neovim && \
-git config --global user.email "hi@kylegrimsrudma.nz" && \
-git config --global user.name "Kyle Grimsrud-Manz" && \
+
+# init git
+$install git tig && \
+git config --global user.email "$email" && \
+git config --global user.name "$name" && \
+
+# install neovim (config in dotfiles step)
+$install neovim && \
+
+# install and configure zsh
+$install curl zsh && \
 clone_if_not_exists https://github.com/wting/autojump $HOME/.zsh/autojump && \
 mkdir -p ~/.zsh/fzf && \
 ([ -d ~/.zsh/fzf/.git ] || git clone https://github.com/junegunn/fzf ~/.zsh/fzf) && \
@@ -43,8 +64,9 @@ if [ "$(getent passwd $(whoami) | cut -d: -f7)" != "$(which zsh)" ]; then
     chsh -s $(which zsh)
 fi && \
 
-$install xmobar fastfetch && \
 
+# install and configure xmonad, xmobar
+$install xmobar fastfetch && \
 clone_if_not_exists https://github.com/vxsl/.xmonad $HOME/.xmonad && \
 cd $HOME/.xmonad && \
 git config --local status.showUntrackedFiles no && \
@@ -55,27 +77,26 @@ if [ ! -f "$HOME/.ghcup/bin/stack" ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
 fi && \
 if [ ! -f "$HOME/.xmonad/stack.yaml" ]; then
-    $HOME/.ghcup/bin/stack init
+    $HOME/.ghcup/bin/stack init && \
+    $HOME/.ghcup/bin/stack install
 fi && \
-cd $HOME/.xmonad && \
-$HOME/.ghcup/bin/stack install
 
-desktop_file="/usr/share/xsessions/xmonad.desktop"
+# register xmonad as DE
 if [ ! -f "$desktop_file" ]; then
     sudo cp $DIRNAME/xmonad.desktop "$desktop_file"
 fi && \
 
+# install convenience scripts
 $install xdotool && \
 clone_if_not_exists https://github.com/vxsl/bin $HOME/bin && \
 
-$install dunst nitrogen arandr xautolock picom xsetroot && \
+# install dotfiles
+$install dunst nitrogen arandr xautolock picom xsetroot xclip && \
 clone_if_not_exists https://github.com/vxsl/.dotfiles $HOME/.dotfiles && \
 $install stow && \
 cd $HOME/.dotfiles && ./setup-stow.sh && \
 
-[ -f ~/.profile ] && grep -q "export EDITOR=nvim" ~/.profile || echo "export EDITOR=nvim" >> ~/.profile
-[ -f ~/.profile ] && grep -q "export GIT_EDITOR=nvim" ~/.profile || echo "export GIT_EDITOR=nvim" >> ~/.profile
-
+# install xob and other volume stuff
 $install python3-pip && \
 pip3 install pulsectl && \
 clone_if_not_exists https://github.com/florentc/xob /usr/local/src/xob --sudo && \
@@ -83,6 +104,7 @@ cd /usr/local/src/xob && \
 $install autoreconf aclocal libX11-devel libXrender-devel libconfig-devel && \
 sudo make && sudo make install && \
 
+# install xidlehook
 $install cargo && \
 clone_if_not_exists https://github.com/jD91mZM2/xidlehook $HOME/dev/xidlehook && \
 cd $HOME/dev/xidlehook && \
@@ -90,12 +112,18 @@ cargo build --release --bins && \
 mkdir -p $HOME/.cargo/bin &&
 cp $HOME/dev/xidlehook/target/release/xidlehook $HOME/.cargo/bin && \
 
+# install snap, misc. snaps
 $install snapd && \
 sudo ln -s /var/lib/snapd/snap /snap && \
 snap install obsidian --classic && \
 snap install code --classic && \
 
+# source .profile
 source $HOME/.profile && \
+
+# install misc. gui progs
 $install firefox alacritty && \
+
+# go home
 cd $HOME && \
 zsh
